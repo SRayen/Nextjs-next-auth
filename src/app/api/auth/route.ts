e8060d2a53d9e51fdaef29bf7fd5eb2449178c26
Selmen Rayen <selmanerayene95@gmail.com>
2023-12-29 01:20:54 +0100

    Integrate next auth

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import { Account, User as AuthUser } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import * as bcrypt from "bcrypt";
+import { User } from "@/models/User";
+import connect from "@/utils/db";
+
+export const authOptions: any = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials: any) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isPasswordCorrect) {
+              return user;
+            }
+          }
+        } catch (err: any) {
+          throw new Error(err);
+        }
+      },
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+  ],
+};
